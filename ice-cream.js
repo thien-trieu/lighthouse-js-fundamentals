bf1c9cee25b6848bf531c2eb891f4b45e74118d4
Thien Trieu <thien.trieu@telus.com>
2022-11-04 19:44:12 -0700

    Adding ice-cream array excercise.

---
+++ ice-cream.js
@@ -0,0 +1,21 @@
+/*
+The freezer at Lighthouse Labs is overflowing with Ice Cream 
+so we need to have an ice cream party for all the students so it can be eaten. 
+But before we can do that, we need determine which flavours of ice cream we have, 
+and we'll need to organize them for easy access.
+*/
+
+
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[5]);
+
+console.log(iceCreamFlavours.length);
